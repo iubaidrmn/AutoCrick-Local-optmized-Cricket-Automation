6f4061fc1228abcf612000a54dc7b35b78e29f3f
Ubaid Ur Rehman <iubaidrmn@gmail.com>
2023-04-14 14:28:37 +0500

    initial commit with sigin page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,74 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialogModule, MatDialogRef } from '@angular/material/dialog';
+import { MatIconModule } from '@angular/material/icon';
+import { MatOptionModule, MatNativeDateModule } from '@angular/material/core';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatSelectModule } from '@angular/material/select';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatStepperModule } from '@angular/material/stepper';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner'
+import { MatBadgeModule } from '@angular/material/badge';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatSliderModule } from '@angular/material/slider';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatTableModule } from '@angular/material/table';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import {MatBottomSheetModule} from '@angular/material/bottom-sheet';
+import { MatCardModule } from '@angular/material/card';
+import { ReactiveFormsModule} from '@angular/forms';
+import { LoginComponent } from './login/login.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatDividerModule,
+    MatButtonModule,
+    MatDialogModule, 
+    MatIconModule,
+    MatOptionModule,
+    MatNativeDateModule,
+    MatRadioModule,
+    MatAutocompleteModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatMenuModule,
+    MatDatepickerModule,
+    MatSelectModule,
+    MatExpansionModule,
+    MatCheckboxModule,
+    MatStepperModule,
+    MatTabsModule,
+    MatProgressSpinnerModule,
+    MatBadgeModule,
+    MatProgressBarModule,
+    MatSliderModule,
+    MatTooltipModule,
+    MatTableModule,
+    MatSidenavModule,
+    MatSnackBarModule,
+    MatPaginatorModule,
+    MatBottomSheetModule,
+    MatCardModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
